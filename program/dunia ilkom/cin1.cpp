af869d1a06b4e5ae3d2acdcd82eea8fa6b7608dc
Aditya Nugraha <adityanugraha795@gmail.com>
2021-05-20 20:18:10 +0800

    tambah materi cin

---
+++ program/dunia ilkom/cin1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int harga;
+ 
+  cout << "Masukkan harga buku: ";
+  cin  >> harga;
+ 
+  cout << "Harga buku C++ adalah: "<< harga << endl;
+  return 0;
+}
