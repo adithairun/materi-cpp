8936d35d613dfb8ee878acfc9ae936642e3f9e1f
Aditya Nugraha <adityanugraha795@gmail.com>
2021-05-20 20:29:57 +0800

    tambah materi array dan programnya

---
+++ program/dunia ilkom/array2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int pecahan[] = {3,4,5,6};
+ 
+  cout << "Isi array pertama : " << pecahan[0] << endl;
+  cout << "Isi array kedua   : " << pecahan[1] << endl;
+  cout << "Isi array ketiga  : " << pecahan[2] << endl;
+  cout << "Isi array ketiga  : " << pecahan[3] << endl;
+  cout << endl;
+ 
+  
+ 
+  return 0;
+}
+
+
