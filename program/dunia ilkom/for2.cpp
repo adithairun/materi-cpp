3d87184e2e6f1cece7766ae93ba9dcd93535a4fe
Aditya Nugraha <adityanugraha795@gmail.com>
2021-05-21 18:53:27 +0800

    tambah materi for dan programnya

---
+++ program/dunia ilkom/for2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int i;
+  for (i = 1; i <= 10; i++) {
+    cout << i*3 << " ";
+  }
+ 
+  return 0;
+}
---
+++ program/dunia ilkom/for3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int i;
+  for (i = 3; i <= 30; i = i + 3) {
+    cout << i << " ";
+  }
+ 
+  return 0;
+}
