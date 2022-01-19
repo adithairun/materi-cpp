b856bb72c2034854a61a3941b68d1908dcd3fdc6
Aditya Nugraha <adityanugraha795@gmail.com>
2022-01-19 21:10:04 +0800

    edit materi

---
+++ program/dunia ilkom/++a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int a = 1;
+cout<< " a Awal = " << a <<endl;
+cout<< " ++a Adalah = " << ++a <<endl;
+cout<< " a Akhir = " << a <<endl;
+}
+
+
---
+++ program/dunia ilkom/a++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int a = 1;
+//cout<< " a Awal = " << a <<endl;
+cout<< " a++ adalah = " << a++ <<endl;
+//cout<< " a Akhir = " << a <<endl;
+}
+
+
+
